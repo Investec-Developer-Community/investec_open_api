8e583891b2892d51d06396ccee837d9a131fdaf9
Steve Bissett <steve.bissett@gmail.com>
2024-04-05 21:19:39 +0200

    Update faraday

--- spec/lib/investec_open_api/client_spec.rb
+++ spec/lib/investec_open_api/client_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe InvestecOpenApi::Client do
           'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
           'Authorization' => 'Basic VGVzdDpTZWNyZXQ=',
           'Content-Type' => 'application/x-www-form-urlencoded',
-          'User-Agent' => 'Faraday v2.7.11',
+          'User-Agent' => 'Faraday v2.9.0',
           'X-Api-Key' => 'TESTKEY'
         })
       .to_return(status: 200, body: {
@@ -40,7 +40,7 @@ RSpec.describe InvestecOpenApi::Client do
             "Accept" => "application/json",
             "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
             "Authorization" => "Bearer 123",
-            "User-Agent" => "Faraday v2.7.11"
+            "User-Agent" => "Faraday v2.9.0"
           }
         )
         .to_return(
@@ -130,7 +130,7 @@ RSpec.describe InvestecOpenApi::Client do
         "Accept" => "application/json",
         "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
         "Authorization" => "Bearer 123",
-        "User-Agent" => "Faraday v2.7.11"
+        "User-Agent" => "Faraday v2.9.0"
       }
     end
 
